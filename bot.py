7321365a1c5b9012b43919acbfdfa6f0a9c85add
SosigFairyHill <lyra.dunseith@gmail.com>
2020-03-17 14:37:01 +0000

    Cleaned up ignore tests

--- bot.py
+++ bot.py
@@ -8,11 +8,6 @@ CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
 
-def to_datetime(datestring):
-    time_tuple = parsedate_tz(datestring.strip())
-    dt = datetime(*time_tuple[:6])
-    return dt - timedelta(seconds=time_tuple[-1])
-
 def tweet_test():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
     auth.set_access_token(ACCESS_KEY, ACCESS_SECRET)
@@ -33,7 +28,7 @@ def tweet_reply():
 
     hashtags = ['#COVID19walkout', '#COVID19WALKOUT', '#covid19walkout']
 
-    reply_text = 'We are a support group for workers affected by COVID19. Please follow us and join the FB group at https://www.facebook.com/groups/329192668038673/'
+    reply_text = 'For support/advice as a worker affected by coronavirus, follow us and join the FB group at www.facebook.com/groups/329192668038673/'
 
     tweet_history = []
 
@@ -42,8 +37,8 @@ def tweet_reply():
         for hashtag in hashtags: # cycle through the hashtags
 
             for tweet in tweepy.Cursor(api.search, q=hashtag, count=100).items(): # cycle through the tweets found with the hashtag
-                # Ignore tweets already replied to, and only look at those in the last hour and in the UK
-                if ( tweet.id not in tweet_history ) and ( tweet.place.country_code == 'GB' ) and ( to_datetime(tweet.created_at) > (datetime.now() - timedelta(hours = 1)) ):
+                # Ignore tweets already replied to, and only look at those in the last hour and in English
+                if ( tweet.id not in tweet_history ) and ( tweet.lang == 'en' ) and ( tweet.created_at > (datetime.now() - timedelta(hours = 1)) ):
                     
                     # Add this tweet to those already replied to
                     tweet_history.append(tweet.id)
@@ -53,7 +48,7 @@ def tweet_reply():
                     # Tweet a reply to the user
                     api.update_status(status=reply_status, in_reply_to_status_id=tweet.id)
 
-        time.sleep(INTERVAL)                
+        time.sleep(interval)                
 
 if __name__ == '__main__':
     tweet_test()
