ee27bafe62ba11f11df72f46234edef5636ae3cf
SosigFairyHill <lyra.dunseith@gmail.com>
2020-03-17 11:05:11 +0000

    Added restrictions on tweet location and time

--- bot.py
+++ bot.py
@@ -1,10 +1,17 @@
 import tweepy, time, sys
 from os import environ
+from datetime import datetime, timedelta
+from email.utils import parsedate_tz
 
 CONSUMER_KEY = environ['CONSUMER_KEY']
 CONSUMER_SECRET = environ['CONSUMER_SECRET']
 ACCESS_KEY = environ['ACCESS_KEY']
 ACCESS_SECRET = environ['ACCESS_SECRET']
+
+def to_datetime(datestring):
+    time_tuple = parsedate_tz(datestring.strip())
+    dt = datetime(*time_tuple[:6])
+    return dt - timedelta(seconds=time_tuple[-1])
 
 def tweet_test():
     auth = tweepy.OAuthHandler(CONSUMER_KEY, CONSUMER_SECRET)
@@ -35,8 +42,9 @@ def tweet_reply():
         for hashtag in hashtags: # cycle through the hashtags
 
             for tweet in tweepy.Cursor(api.search, q=hashtag, count=100).items(): # cycle through the tweets found with the hashtag
-                # Ignore tweets already replied to. NB Can edit to ignore users already replied to
-                if ( tweet.id not in tweet_history ):
+                # Ignore tweets already replied to, and only look at those in the last hour and in the UK
+                if ( tweet.id not in tweet_history ) and ( tweet.place.country_code == 'GB' ) and ( to_datetime(tweet.created_at) > (datetime.now() - timedelta(hours = 1)) ):
+                    
                     # Add this tweet to those already replied to
                     tweet_history.append(tweet.id)
                     
